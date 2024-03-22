01e0f45f210b4d8f869489b062f05235abb42b66
Taranjot Singh <140800202+Taranjot13@users.noreply.github.com>
2024-03-22 19:47:33 +0530

    Update #7.c

--- #7.c
+++ #7.c
@@ -1,5 +1,4 @@
-//If a five-digit number is input through the keyboard, write a program to calculate the sum of its digits. (Hint: Use the modulus
-operator ‘%’).
+//If a five-digit number is input through the keyboard, write a program to calculate the sum of its digits. (Hint: Use the modulus operator ‘%’).
 #include <stdio.h>
 int main()
 {

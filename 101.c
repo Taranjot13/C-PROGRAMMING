517168582fb8f9572b5b7716486446b718c06ccd
Taranjot Singh <140800202+Taranjot13@users.noreply.github.com>
2024-04-02 09:39:23 +0530

    Create 101.c

---
+++ 101.c
@@ -0,0 +1,20 @@
+// C program to enter elements in a 2D-array and print thr array.
+
+#include <stdio.h>
+int main(){
+    int row,col,n=2,m=2;
+    int A[2][2];
+    for(row=0;row<n;row++){
+        for(col=0;col<m;col++){
+            printf("Enter element A[%d][%d]",row,col);
+            scanf("%d",&A[row][col]);
+        }
+    }
+    for(row=0;row<n;row++){
+            printf("[");
+        for(col=0;col<m;col++){
+           printf("%d\t",A[row][col]);
+    }printf("]\n");
+    }
+    return 0;
+}

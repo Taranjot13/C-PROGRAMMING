85c85cf03f95997a3b80261f1fba8562918a0a46
Taranjot Singh <140800202+Taranjot13@users.noreply.github.com>
2024-04-18 05:39:05 +0000

    create file

---
+++ #120.c
@@ -0,0 +1,14 @@
+/*
+Write a program that converts all lowercase characters in a given string to its equivalent uppercase character.Write a program that converts all lowercase characters in a given string to its equivalent uppercase character.
+*/
+
+#include<stdio.h>
+#include<string.h>
+void main() {
+char str[100];
+gets(str);
+printf("\n\n");
+strupr(str);
+puts(str);
+getch();
+}
